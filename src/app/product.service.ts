5422ee3643f6889daf2df79a2d9e4d5aca1a7903
Michael Robeson <michael.robeson@hcahealthcare.com>
2018-01-14 16:46:57 -0600

    Tweaked exercise 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angluar/http'
+import { Http, Response } from '@angular/http';
 import 'rjxs/add/operator/map';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl)
+    return this._http.get(this._albumUrl).map((response) =>
+      response.json)
   }
-
 }
